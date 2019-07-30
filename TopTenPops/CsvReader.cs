6c9efbb1a9af527e7450c8bb18dc6d87b293a4f7
Alex Hilton <ahilton@clearent.com>
2019-07-30 10:37:40 -0500

    Assembled from Tutorial

---
+++ TopTenPops/CsvReader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TopTenPops
+{
+    class CsvReader
+    {
+        private string _csvFilePath;
+
+        public CsvReader(string csvFilePath)
+        {
+            _csvFilePath = csvFilePath;
+        }
+
+        public Country[] ReadFirstNCountries(int nCountires)
+        {
+            Country[] countries = new Country[nCountires];
+                        
+            using (StreamReader sr = new StreamReader(_csvFilePath))
+            {
+                // read header line
+                sr.ReadLine();
+
+                for (int i = 0; i < nCountires; i++)
+                {
+                string csvLine = sr.ReadLine();
+                countries[i] = ReadCountryFromCsvLine(csvLine);
+                }
+            }
+            return countries;
+        }
+
+        public Country ReadCountryFromCsvLine(string csvLine)
+        {
+            string[] parts = csvLine.Split(',');
+
+            string name = parts[0];
+            string code = parts[1];
+            string region = parts[2];
+            int population = int.Parse(parts[3]);
+
+            return new Country(name, code, region, population);
+        }
+    }
+}
